7e89eb8948c9e79d7070343f69a853c7d8cb3b7b
victor de souza magaldi <victmagaldi@hotmail.com>
2022-05-16 22:49:57 -0300

    Increment

--- src/pages/Redux.tsx
+++ src/pages/Redux.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export function Redux() {
   const state = useSelector((state) => state);
+  const dispatch = useDispatch();
   console.log("state", state);
-  return <div>Redux</div>;
+  const increment = () => {
+    dispatch({ type: "INCREMENTAR" });
+  };
+  return (
+    <div>
+      Redux
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
 }
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 // Reducer criado da mesma forma
-function reducer(state = 0, action) {
+function reducer(state = 0, action: { type: "INCREMENTAR" }) {
   switch (action.type) {
     case "INCREMENTAR":
       return state + 1;
@@ -11,4 +11,5 @@ function reducer(state = 0, action) {
 }
 export default configureStore({
   reducer: reducer,
+  devTools: true
 });
