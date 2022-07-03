d214db6dc607cb33496a672e1ed0f54514938013
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-02 23:00:37 -0300

    test react timer

--- src/pages/ReactTimerPickerPage.tsx
+++ src/pages/ReactTimerPickerPage.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+import TimePicker from "react-time-picker";
 
 export const ReactTimerPickerPage = () => {
+  const [value, onChange] = useState("10:00");
+  console.log("on", onChange)
+
   return (
-    <div>ReactTimerPickerPage</div>
-  )
-}
+    <div>
+      <h3>react-timer-picker error VITE </h3>
+      {/* <TimePicker onChange={ e=>{console.log("e", e)}} value={value} /> */}
+    </div>
+  );
+};
