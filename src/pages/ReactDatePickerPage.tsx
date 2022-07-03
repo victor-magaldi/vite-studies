8a53d6c5b158b36d501b66345f8406c831e30617
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-02 22:46:03 -0300

    react datepicker page

--- src/pages/ReactDatePickerPage.tsx
+++ src/pages/ReactDatePickerPage.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
+import {setDefaultLocale} from "react-datepicker";
+
 import "react-datepicker/dist/react-datepicker.css";
 
 export const DatePickerPage = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState( new Date());
 
   return (
     <div>
       <h3>React-datepicker</h3>
       <DatePicker
+        showTimeSelect
+        timeIntervals={15}
         selected={startDate}
         onChange={(date: Date) => setStartDate(date)}
+        dateFormat="MMMM d, yyyy h:mm aa"
+        formatWeekDay={(nameOfDay) => <span>{nameOfDay.substr(0, 3)}</span>}
       />
     </div>
   );
