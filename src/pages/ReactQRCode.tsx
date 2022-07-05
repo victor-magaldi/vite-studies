96b97ce1bf79b7edbe7e4a1432c3aa2a9cf168d5
victor de souza magaldi <victmagaldi@hotmail.com>
2022-07-05 18:39:09 -0300

    using lib qr code

--- src/pages/ReactQRCode.tsx
+++ src/pages/ReactQRCode.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import QRCode from "react-qr-code";
 
 export const ReactQRCode = () => {
   return (
-    <div>ReactQRCode</div>
-  )
+    <div>
+      <h5>React qr code</h5>
+      <QRCode
+        size={256}
+        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+        value={"https://github.com/rosskhanas/react-qr-code#readme"}
+      />
+    </div>
+  );
 }
