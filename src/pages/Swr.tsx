ee211405b8caf078a5a2d73435b972ea54f7d9bb
victor de souza magaldi <victmagaldi@hotmail.com>
2022-10-26 20:00:15 -0300

    first data Fetch

--- src/pages/Swr.tsx
+++ src/pages/Swr.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 export const Swr = () => {
-  return <div>Swr</div>;
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+  console.log("data=>", data);
+
+  if (error) return <div>falhou em carregar</div>;
+  if (!data) return <div>carregando...</div>;
+  return <div>Olá {data.name}!</div>;
 };
