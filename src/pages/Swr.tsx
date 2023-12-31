4556458b0837b5132af1f49d5ba4159065fd6423
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2023-12-31 10:56:29 -0300

    search content swr

--- src/pages/Swr.tsx
+++ src/pages/Swr.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import useSWR from "swr";
 
+type TypePost = {
+  title: string;
+  id: number;
+  userId: number;
+};
+
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 export const Swr = () => {
@@ -12,5 +18,20 @@ export const Swr = () => {
 
   if (error) return <div>falhou em carregar</div>;
   if (!data) return <div>carregando...</div>;
-  return <div>Olá {data.name}!</div>;
+  return (
+    <div>
+      <h3>Buscando dados com swr</h3>
+      {data.map((post: TypePost) => {
+        return (
+          <>
+            <div>
+              <p>{post?.title}</p>
+              <span>userId: {post.userId}</span>
+            </div>
+            <hr />
+          </>
+        );
+      })}
+    </div>
+  );
 };
